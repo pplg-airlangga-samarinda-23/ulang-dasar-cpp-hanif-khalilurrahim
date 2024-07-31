82a2c6233dbcad60642ebebc1c21bb69f7deba17
hanif.khalilurrahim3@gmail.com <hanif.khalilurrahim3@gmail.com>
2024-07-31 11:18:57 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void cetakData(string nama, string nisn, string jurusan, string tinggibadan)
+{
+// cetak data-data nama, nisn, jurusan, tinggi badan
+cout << "nama : " << nama << endl;
+cout << "nisn : " << nisn << endl;
+cout << "jurusan : " << jurusan << endl;
+cout << "tinggi badan : " << tinggibadan << endl;
+}
+
 int main() {
     cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+//buat variabel untuk meyimpan 
+//nama
+//nisn
+//jurusan
+//tinggi
+string nama="hanif khalilurrahim", nisn="08190727321", jurusan="PPLG", tinggibadan="170";
+
+cetakData(nama, nisn, jurusan, tinggibadan);
+
+return 0;
 }
